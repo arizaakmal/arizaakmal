a174d37a119fac50a190a70a3ed19a70c755a975
arizaakmal <arizaakmal13@gmail.com>
2022-08-01 07:39:51 +0700

    modified footer and others

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  $("span").fadeTo(1000, 0.5);
+  $(".fadeTo").fadeTo(1000, 0.5);
+  $(".fadeTo2").fadeTo(1000, 0.8);
+  //make navbar bg-dark on scroll
+  $(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+      $("nav").addClass("bg-dark");
+    } else {
+      $("nav").removeClass("bg-dark");
+    }
+  });
 });
