3779da4163d8fae0ebd09273e4a42f2be12d3127
arizaakmal <arizaakmal04@gmail.com>
2022-08-09 11:50:00 +0700

    penyesuaian responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-  $(".fadeTo").fadeTo(1000, 0.5);
-  $(".fadeTo2").fadeTo(1000, 0.8);
   //make navbar bg-dark on scroll
   $(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
@@ -9,4 +7,23 @@ $(document).ready(function () {
       $("nav").removeClass("bg-dark");
     }
   });
+
+  $(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+  // fadeTo dijalankan setelah 3s
+
+  $("#fadeTo").fadeTo(3000, 0.3);
 });
