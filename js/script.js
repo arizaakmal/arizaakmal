5eb7e9fdbbcf7dc6d89e4fe8ad2004e229c17e04
arizaakmal <arizaakmal04@gmail.com>
2023-02-14 10:33:43 +0700

    add scroll-top button

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ $(document).ready(function () {
 
   $(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
+      $(".scroll-top").fadeIn(1000);
+      $(".scroll-top").removeClass("visually-hidden");
       $("nav").addClass("bg-dark");
     } else {
       $("nav").removeClass("bg-dark");
+      //$(".scroll-top").fadeIn(1000);
+      $(".scroll-top").addClass("visually-hidden");
     }
   });
 
@@ -30,4 +34,56 @@ $(document).ready(function () {
   });
   // fadeTo dijalankan setelah 3s
   $("#fadeTo").fadeTo(3000, 0.3);
+
+  //make scroll top button
+  // $(window).scroll(function () {
+
+  //   if ($(this).scrollTop() > 100) { //
+  //     $(".scroll-top").fadeIn();
+  //   } else {
+  //     $(".scroll-top").fadeOut();
+  //   }
+  // });
+
+  //jika berada di section dengan id home tombol scroll-top akan hilang sedangkan jika berada di halaman lain tombol scroll-top akan muncul dan jika di klik akan scroll ke atas
+
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 100) {
+  //     $(".scroll-top").fadeIn();
+  //   } else {
+  //     $(".scroll-top").fadeOut();
+  //   }
+  // });
+
+  /**
+   * Scroll top button
+   */
+  // const $scrollTop = $(".scroll-top");
+  // if ($scrollTop.length) {
+  //   $(window).on("scroll", function () {
+  //     if ($(this).scrollTop() > 100) {
+  //       $scrollTop.toggle("visually-hidden");
+  //     } else {
+  //       $scrollTop.fadeOut();
+  //     }
+  //   });
+
+  //   $scrollTop.on("click", function () {
+  //     $("html, body").animate(
+  //       {
+  //         scrollTop: 0,
+  //       },
+  //       500
+  //     );
+  //   });
+  // }
+
+  //ganti kode scroll top button di atas dengan jquery
+  // $(window).scroll(function () {
+  //   if ($(this).scrollTop() > 100) {
+  //     $(".scroll-top").fadeIn();
+  //   } else {
+  //     $(".scroll-top").fadeOut();
+  //   }
+  // });
 });
